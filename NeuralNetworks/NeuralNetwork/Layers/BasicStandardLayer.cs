f84dabed552fca58ea40130b45f9c8ad061a999d
Joel zhu <joel.zhu@grenoble-inp.org>
2021-02-01 18:18:16 +0100

    opti standard layer

--- NeuralNetworks/NeuralNetwork/Layers/BasicStandardLayer.cs
+++ NeuralNetworks/NeuralNetwork/Layers/BasicStandardLayer.cs
@@ -25,7 +25,7 @@ namespace NeuralNetwork.Layers
         public Matrix<double> Zeta { get; set; }
         public Matrix<double> B_Rond { get; set; }
         public Matrix<double> Alpha { get; set; }
-        //public Matrix<double> Grad_Weight { get; set; } // L2
+        public Matrix<double> Grad_Weight { get; set; }
         public Matrix<double> Mat_Un { get; set; } // matrice de un (summary page 7 cours 3)
 
 
@@ -37,26 +37,16 @@ namespace NeuralNetwork.Layers
             Activation = Matrix<double>.Build.Dense(LayerSize, BatchSize);
 
             // attributs
-            this.Bias = bias.Clone();
-            for (int i = 0; i < this.BatchSize-1; ++i)
-            {
-                this.Bias = this.Bias.Append(bias);
-            }
-
-            this.Weights = weights;
-            this.Activator = activator;
-            this.LearningParameter = learningParameter;
-            this.Alpha = Matrix<double>.Build.Dense(InputSize, batchSize);
-
-
             this.Mat_Un = Matrix<double>.Build.Dense(this.BatchSize, 1);
             Mat_Un.Multiply(0, Mat_Un);
             Mat_Un.Add(1, Mat_Un);
 
-
-            // L2
-            //this.Grad_Weight = weights;
-            //this.Grad_Weight.Multiply(0, this.Grad_Weight);
+            this.Bias = bias.TransposeAndMultiply(Mat_Un);
+            this.Weights = weights;
+            this.Activator = activator;
+            this.LearningParameter = learningParameter;
+            this.Alpha = Matrix<double>.Build.Dense(InputSize, batchSize);
+            this.Grad_Weight = Matrix<double>.Build.Dense(weights.RowCount, weights.ColumnCount);
         }
 
         public void BackPropagate(Matrix<double> upstreamWeightedErrors)
@@ -75,15 +65,10 @@ namespace NeuralNetwork.Layers
 
         public void UpdateParameters()
         {
-            var Grad_Bias = this.B_Rond.Multiply(this.Mat_Un);
-            var Grad_Bias_Clone = Grad_Bias.Clone();
-            for (int i = 0; i < this.BatchSize - 1; ++i)
-            {
-                Grad_Bias = Grad_Bias.Append(Grad_Bias_Clone);
-            }
-
+            var Grad_Bias = this.B_Rond.Multiply(this.Mat_Un).TransposeAndMultiply(Mat_Un);
+            
             //this.Grad_Weight.Add(this.Alpha.TransposeAndMultiply(this.B_Rond), this.Grad_Weight); // L2
-            var Grad_Weight = this.Alpha.TransposeAndMultiply(this.B_Rond);
+            this.Alpha.TransposeAndMultiply(this.B_Rond, this.Grad_Weight);
 
             this.Weights.Subtract(Grad_Weight.Multiply(this.LearningParameter.LearningRate/this.BatchSize), this.Weights);
             this.Bias.Subtract(Grad_Bias.Multiply(this.LearningParameter.LearningRate/this.BatchSize), this.Bias);
